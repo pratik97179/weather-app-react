ed907a896a5fa6376a62d3b05ffd1efe9715debf
JATIN'S MACHINE <jatinjaiswal56@gmail.com>
2023-10-11 02:16:34 +0530

    Weather Cards Added

--- src/component/weather-app/Weather.jsx
+++ src/component/weather-app/Weather.jsx
@@ -8,63 +8,80 @@ import HeaderBar from "./HeaderBar.jsx";
 import PlaceOverviewCard from "./PlaceOverviewCard.jsx";
 
 const Weather = () => {
-    const [forecast, setForecast] = useState({});
+  const [forecast, setForecast] = useState({});
 
-    useEffect(() => {
-        fetchWeather().then((result) => {
-            setForecast(result);
-            console.log(result);
-        });
-    }, []);
+  useEffect(() => {
+    fetchWeather().then((result) => {
+      setForecast(result);
+      console.log(result);
+    });
+  }, []);
 
-    async function fetchWeather() {
-        try {
-            let response = await fetch(
-                "http://api.weatherapi.com/v1/forecast.json?key=711ba0ec77e3441bab3124346231409&q=Bangalore&days=5&aqi=no&alerts=no"
-            );
-            let res = await response.json();
-            return res;
-        } catch (e) {
-            console.log(e);
-        }
+  async function fetchWeather() {
+    try {
+      let response = await fetch(
+        "http://api.weatherapi.com/v1/forecast.json?key=711ba0ec77e3441bab3124346231409&q=Bangalore&days=5&aqi=no&alerts=no"
+      );
+      let res = await response.json();
+      return res;
+    } catch (e) {
+      console.log(e);
     }
+  }
 
-    return (
-        <div className="body-card">
-            <HeaderBar
-                weatherText={
-                    forecast["current"] != null
-                        ? forecast["current"]["condition"]["text"]
-                        : "Loading..."
-                }
-            />
-            <PlaceOverviewCard
-                placeName={
-                    forecast["location"] != null
-                        ? `${forecast["location"]["name"]}, ${forecast["location"]["region"]}`
-                        : "Loading..."
-                }
-                currentTemperature={
-                    forecast["current"] != null
-                        ? forecast["current"]["temp_c"]
-                        : ""
-                }
-                placeImage={
-                    forecast["current"] != null
-                        ? forecast["current"]["condition"]["icon"]
-                        : "https://images.unsplash.com/photo-1615485020471-b66207f5e3bb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80"
-                }
-            />
-            <div class="weather-card">
-            <WeatherCard />
-            <WeatherCard />
-            <WeatherCard />
-            <WeatherCard />
-            </div>
+  return (
+    <div className="body-card">
+      <HeaderBar
+        weatherText={
+          forecast["current"] != null
+            ? forecast["current"]["condition"]["text"]
+            : "Loading..."
+        }
+      />
+      <PlaceOverviewCard
+        placeName={
+          forecast["location"] != null
+            ? `${forecast["location"]["name"]}, ${forecast["location"]["region"]}`
+            : "Loading..."
+        }
+        currentTemperature={
+          forecast["current"] != null ? forecast["current"]["temp_c"] : ""
+        }
+        placeImage={
+          forecast["current"] != null
+            ? forecast["current"]["condition"]["icon"]
+            : "https://images.unsplash.com/photo-1615485020471-b66207f5e3bb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80"
+        }
+      />
+      <div className="weather-card">
+        <WeatherCard
+          type={"Temperature"}
+          action={forecast["current"] != null && `${forecast["current"]["temp_c"]}°C`}
+        />
+        <WeatherCard
+          type={"Wind Speed"}
+          action={
+            forecast["current"] != null && `${forecast["current"]["wind_kph"]} km/h`
+          }
+        />
+        <WeatherCard
+          type={"Humidity"}
+          action={
+            forecast["current"] != null && `${forecast["current"]["humidity"]} %`
+          }
+        />
+        <WeatherCard
+          type={"Pressure"}
+          isLast={true}
+          action={
+            forecast["current"] != null && `${forecast["current"]["pressure_in"]}mm`
+          }
+        />
+      </div>
 
-            <WeatherDescription />
-        </div>
-    );
+      <WeatherDescription />
+    </div>
+  );
 };
 
 export default Weather;
